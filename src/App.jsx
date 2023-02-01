b643a5f4282fe0fc566f3e8474c9da8e773aef4e
SShipon <nxshipon@gmail.com>
2023-02-02 00:34:40 +0600

    code your

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente odit corrupti voluptate quod? Deserunt nesciunt placeat ad, rem numquam quam.</p>
+        </div>
+    );
+};
+
+export default App;
